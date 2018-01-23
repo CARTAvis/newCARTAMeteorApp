c4cd6b7df4bbc951046199dfc0573b0bd56e8fd8
markccchiang <chengchin.chiang@gmail.com>
2018-01-23 11:23:43 +0800

    refactor the Clipping class.

--- imports/clipping/Clipping.js
+++ imports/clipping/Clipping.js
@@ -11,81 +11,24 @@ class Clipping extends Component {
     // this.props.dispatch(actions.setupClipping(this.state.percentile[9]));
   }
 
-  clip0 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[0]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip1 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[1]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip2 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[2]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip3 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[3]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip4 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[4]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip5 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[5]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip6 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[6]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip7 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[7]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip8 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[8]));
-    this.props.dispatch(actions.getClipState());
-  }
-  clip9 = () => {
-    this.props.dispatch(actions.updateClipping(this.state.percentile[9]));
+  handleClip = (percentile) => {
+    this.props.dispatch(actions.updateClipping(percentile));
     this.props.dispatch(actions.getClipState());
   }
 
   render() {
+    const percentiles = this.state.percentile;
+    let percentileButtons = null;
+    percentileButtons = percentiles.map(percentile =>
+      (<button className="button" onClick={() => this.handleClip(percentile)} type="button">
+        {`${percentile * 100}%`}
+      </button>),
+    );
+
     return (
       <div>
         <h3>Clipping for percentile:</h3>
-        <button className="button" onClick={this.clip0} type="button">
-          {`${this.state.percentile[0] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip1} type="button">
-          {`${this.state.percentile[1] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip2} type="button">
-          {`${this.state.percentile[2] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip3} type="button">
-          {`${this.state.percentile[3] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip4} type="button">
-          {`${this.state.percentile[4] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip5} type="button">
-          {`${this.state.percentile[5] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip6} type="button">
-          {`${this.state.percentile[6] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip7} type="button">
-          {`${this.state.percentile[7] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip8} type="button">
-          {`${this.state.percentile[8] * 100}%`}
-        </button>
-        <button className="button" onClick={this.clip9} type="button">
-          {`${this.state.percentile[9] * 100}%`}
-        </button>
+        {percentileButtons}
       </div>
     );
   }
